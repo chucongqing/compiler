adfccbb1e74ce3eea36421d193fe47a75ec3441c
chucongqing <chucongqing@gmai.com>
2021-06-04 00:00:10 +0800

    6.2

--- src/go/vm/inst_load.go
+++ src/go/vm/inst_load.go
@@ -2,6 +2,17 @@ package vm
 
 import . "golua/api"
 
+func loadNil(i Instruction, vm LuaVM) {
+	a, b, _ := i.ABC()
+	a += 1
+
+	vm.PushNil()
+	for i := a; i <= a+b; i++ {
+		vm.Copy(-1, i)
+	}
+	vm.Pop(1)
+}
+
 func loadBool(i Instruction, vm LuaVM) {
 	a, b, c := i.ABC()
 	a += 1
